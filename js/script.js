8ccfa33ab858dac11d64de9b243afb6ba0991700
Bill Ramsey <bill@billiamram.com>
2013-02-13 19:24:57 -0500

    added pushState to projects page.

--- js/script.js
+++ js/script.js
@@ -1,5 +1,19 @@
 !function ($) {
 
+    // Load project page based on uri hash.
+    if (window.location.hash) {
+        console.log('hash: ' + window.location.hash);
+        var link = $('.preview[href*=' + window.location.hash.slice(1) + ']')[0];
+        console.log(link);
+        if (link) {
+            loadProject(link, function($project) {
+                $project.fadeIn('fast');
+                $('.preview').hide();
+            });
+        }
+    }
+
+    // Pre-load project pages once the main page is done.
     $(window).bind('load', function() {
         var $projectsContainer = $('#projects > .container');
         $('.preview').each(function(i, link) {
@@ -8,32 +22,14 @@
     });
 
     $(function () {
-        /*
-      $(".carousel").jCarouselLite({
-            btnNext: ".next",
-            btnPrev: ".prev",
-        scroll: 1,
-        visible: 1,
-        circular: false,
-        afterEnd: function(a) {
-              if(a[0].id == "first") {
-            $('.prev').fadeOut('fast');
-            $('.next').fadeIn('fast');
-          } else if(a[0].id == "last") {
-            $('.prev').fadeIn('fast');
-            $('.next').fadeOut('fast');
-          } else {
-            $('.prev').fadeIn('fast');
-            $('.next').fadeIn('fast');
-          }
-          }
-        });
-        */
-
       $('.preview').bind('click', maximizeClick);
+
+      // Escape maximized project back to the main page.
       $('#projects').on('click', '.project', function(e) {
           $(this).closest('.project').hide();
           $('.preview').fadeIn('fast');
+
+          window.history.pushState(null, null, "projects.html");
 
           if ($(e.target).attr('class') === 'back') {
               return false;
@@ -42,6 +38,7 @@
 
     });
 
+    // Load's a project from the clicked url.
     function loadProject(link, callback) {
         var $project,
             projectId = $(link).attr('data-project-id');
@@ -49,8 +46,7 @@
         if (projectId === undefined) {
             $.get(link.href, function(result) {
                 $project = $(result).find('.project');
-                $project.css('cursor', 'pointer');
-                $project.hide();
+                $project.css({'cursor': 'pointer', 'display': 'none'});
                 $(link).closest('.container').append($project);
                 $(link).attr('data-project-id', $project.attr('id'));
                 if (callback) callback($project);
@@ -67,6 +63,7 @@
         loadProject(this, function($project) {
             $project.fadeIn('fast');
             $('.preview').hide();
+            window.history.pushState(null, null, "projects.html#" + $project.attr('id'));
         });
     }
 }(window.jQuery);
