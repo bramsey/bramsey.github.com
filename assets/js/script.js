c486ba2a5fa120777483527d237fb686f07caa6f
Bill Ramsey <bill@billiamram.com>
2013-02-15 21:59:08 -0500

    changed project structure and css and the corresponding js

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,34 +3,32 @@
     // Load project page based on uri hash.
     if (window.location.hash) {
         console.log('hash: ' + window.location.hash);
-        var link = $('.preview[href*=' + window.location.hash.slice(1) + ']')[0];
-        console.log(link);
+        var link = $('.project-previews__preview[href*=' + window.location.hash.slice(1) + ']')[0];
         if (link) {
             loadProject(link, function($project) {
                 $project.fadeIn('fast');
-                $('.preview').hide();
+                $('.project-previews').hide();
             });
         }
     }
 
     // Pre-load project pages once the main page is done.
     $(window).bind('load', function() {
-        var $projectsContainer = $('#projects > .container');
-        $('.preview').each(function(i, link) {
+        $('.project-previews__preview').each(function(i, link) {
             loadProject(link);
         });
     });
 
     $(function () {
-      $('.preview').bind('click', maximizeClick);
+      $('.project-previews__preview').bind('click', maximizeClick);
 
       // Escape maximized project back to the main page.
-      $('#projects').on('click', '.project', function(e) {
+      $('.projects').on('click', '.project', function(e) {
         var $project = $(this).closest('.project');
         $project.hide();
-        $('.preview').fadeIn('fast');
+        $('.project-previews').fadeIn('fast');
 
-        $.scrollTo($('.preview[data-project-id*=' + $project.attr('id') + ']').offset().top - 16);
+        $.scrollTo($('.project-previews__preview[data-project-id*=' + $project.attr('id') + ']').offset().top - 16);
         window.history.pushState(null, null, "projects.html");
 
         if ($(e.target).attr('class') === 'back') {
@@ -40,7 +38,7 @@
 
     });
 
-    // Load's a project from the clicked url.
+    // Loads a project from the clicked url.
     function loadProject(link, callback) {
         var $project,
             projectId = $(link).attr('data-project-id');
@@ -49,7 +47,7 @@
             $.get(link.href, function(result) {
                 $project = $(result).find('.project');
                 $project.css({'cursor': 'pointer', 'display': 'none'});
-                $(link).closest('.container').append($project);
+                $(link).closest('.projects').append($project);
                 $(link).attr('data-project-id', $project.attr('id'));
                 if (callback) callback($project);
             });
@@ -64,7 +62,7 @@
 
         loadProject(this, function($project) {
             $project.fadeIn('fast');
-            $('.preview').hide();
+            $('.project-previews').hide();
             //$.scrollTo($project);
             $.scrollTo(0);
             window.history.pushState(null, null, "projects.html#" + $project.attr('id'));
